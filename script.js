bc2f8bab300fa687fa69beca00b67c682446e266
tayash-git <14TA0522@amal.se>
2025-12-18 12:34:50 +0100

    penis

---
+++ script.js
@@ -0,0 +1,15 @@
+function openPage(){
+    input = document.getElementById("inputsite");
+
+    if (input.value == "Webbens Historia") {
+        window.open("Webben.html","_self")
+    } else if (input.value == "HTTP/HTTPS") {
+        window.open("HTTP.html","_self")
+    } else if (input.value == "Copyright") {
+        window.open("Copyright.html","_self")
+    } else if (input.value == "DNS") {
+        window.open("DNS.html","_self")
+    }  else if (input.value == "Programmeringens Historia") {
+        window.open("ProgrammeringHistoria.html","_self")
+    }
+}
